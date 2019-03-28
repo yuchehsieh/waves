7a533be5a4e68895f0cdf6d7d5f5c9d07427ff5c
yuchehseih <hsiehdanny860605@gmail.com>
2019-03-28 23:28:21 +0800

    [很重要] 在 .env 設定 database 的 port。然後在 server.js 透過 require('dotenv') 使用，並且搭配 mongoose 去 connect 資料庫

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+require('dotenv').config();
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 2000;
 
 app.listen(port, () => {
-    console.log(`Server Running at ${port}`);
+  console.log(`Server Running at ${port}`);
 });

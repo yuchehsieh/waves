629a2b455838ef60cbcae02d548a76ab3c1ebdf9
yuchehseih <hsiehdanny860605@gmail.com>
2019-03-30 22:33:28 +0800

    [重要] mongoose schema 的自訂 methods。注意必須要用 ES5 的 funcion 宣告，不能用 => 下去寫！！！

--- server/server.js
+++ server/server.js
@@ -30,6 +30,36 @@ app.post('/api/users/register', (req, res) => {
   });
 });
 
+app.post('/api/users/login', (req, res) => {
+  // find the email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: 'Auth failes, email not found'
+      });
+
+    // check password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: 'Wrong password'
+        });
+      // generate a token
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie('w_auth', user.token)
+          .status(200)
+          .json({
+            loginSuccess: true
+          });
+      });
+    });
+  });
+});
+
 const port = process.env.PORT || 2000;
 
 app.listen(port, () => {

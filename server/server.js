5287155774849644ce296bba1c76e380c4bfbc32
yuchehseih <hsiehdanny860605@gmail.com>
2019-04-01 13:54:15 +0800

    [重要] 利用 auth middlware 和 finsOneAnsUpdate 清空 token 實現登出的功能

--- server/server.js
+++ server/server.js
@@ -76,6 +76,15 @@ app.post('/api/users/login', (req, res) => {
   });
 });
 
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user.id }, { token: '' }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
+    });
+  });
+});
+
 const port = process.env.PORT || 2000;
 
 app.listen(port, () => {

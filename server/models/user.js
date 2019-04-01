d93998558ea73ddba3340cfe4a50cb2f81ae9182
yuchehseih <hsiehdanny860605@gmail.com>
2019-04-01 12:10:36 +0800

    [重要] 切換分頁利用儲存的 cookie 去檢查是否認證，建立一個 middleware 首先檢查，透過 mongoose schema 的自訂 static methods 去寫（一樣不能使用 =>）

--- server/models/user.js
+++ server/models/user.js
@@ -87,6 +87,18 @@ userSchema.methods.generateToken = function(cb) {
   //   });
 };
 
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+
+  // decode the token, using jwt
+  jwt.verify(token, process.env.SECRET, function(err, decode) {
+    user.findOne({ _id: decode, token: token }, function(err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };

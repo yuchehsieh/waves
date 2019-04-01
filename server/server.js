429d51e6085a981c51150bd92f07a4e88bf1dd7f
yuchehseih <hsiehdanny860605@gmail.com>
2019-04-01 15:32:01 +0800

    [重要] 把前面所學的再複習一次應用在 brands 上，包含：mongoose: Schema, models, express: moiddle ware(連接一個以上), post/ get request

--- server/server.js
+++ server/server.js
@@ -15,9 +15,31 @@ app.use(cookieParser());
 
 // Models
 const { User } = require('./models/user');
+const { Brand } = require('./models/brand');
 
 // Middlewares
 const { auth } = require('./middleware/auth');
+const { admin } = require('./middleware/admin');
+
+//==============================
+//            BRAND
+//==============================
+
+app.post('/api/product/brand', auth, admin, (req, res) => {
+  const brand = new Brand(req.body);
+
+  brand.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({ success: true, brand: doc });
+  });
+});
+
+app.get('/api/products/brands', (req, res) => {
+    Brand.find({}, (err, brands) => {
+        if(err) return res.status(400).send(err);
+        res.status(200).send(brands)
+    })
+})
 
 //==============================
 //            USER
